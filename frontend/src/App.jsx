f7112788745043275e8ab394ce6f6ca956a0a7b5
Youssif-gs <23p0382@eng.asu.edu.eg>
2026-04-27 01:20:26 +0300

    StudentRecords page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
-
+import StudentRecords from "./Pages/StudentRecords";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/StudentRecords" element={<StudentRecords />} />
       </Routes>
     </BrowserRouter>
   );
